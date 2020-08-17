b85aab67d0006dba8d939eed144846fd35d3fc2f
Prabhuram <prabhuram3739@gmail.com>
2020-08-17 14:49:45 +0530

    debug statement

--- src/apis/youtubeSearch.js
+++ src/apis/youtubeSearch.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 // put your api keys in .env file you can get those here - https://developers.google.com/youtube/v3/getting-started
 export const selectRandomKey = () => {
-  const keys = ["197403268303-jhflf9rivl2bl6mi4fahurtnu7vounpa.apps.googleusercontent.com, 9H9q473RdFxkr6SiSa9ARkQt, AIzaSyBQOrIXNurhz4htG_ThVh-hQGbHCSAb_8U"] //we are splitting the api keys to make an array
+  const keys = ["AIzaSyBQOrIXNurhz4htG_ThVh-hQGbHCSAb_8U"] //we are splitting the api keys to make an array
   const random = Math.floor(Math.random() * Math.floor(keys.length)); //this will get a random number
+  console.log("Random Value:", random);
+  console.log("Keys random:", keys[random]);
   return keys[random];
 };
 
