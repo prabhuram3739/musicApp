7a0cfc8490fcb06bb6b2005e45b5d47e2596950d
Prabhuram <prabhuram3739@gmail.com>
2020-08-17 14:38:05 +0530

    added the client id

--- src/apis/youtubeSearch.js
+++ src/apis/youtubeSearch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // put your api keys in .env file you can get those here - https://developers.google.com/youtube/v3/getting-started
 export const selectRandomKey = () => {
-  const keys = ["AIzaSyBQOrIXNurhz4htG_ThVh-hQGbHCSAb_8U"]; //we are splitting the api keys to make an array
+  const keys = ["197403268303-jhflf9rivl2bl6mi4fahurtnu7vounpa.apps.googleusercontent.com, 9H9q473RdFxkr6SiSa9ARkQt, AIzaSyBQOrIXNurhz4htG_ThVh-hQGbHCSAb_8U"] //we are splitting the api keys to make an array
   const random = Math.floor(Math.random() * Math.floor(keys.length)); //this will get a random number
   return keys[random];
 };
@@ -14,6 +14,5 @@ export default axios.create({
     videoCategoryId: "10",
     type: "video",
     key: selectRandomKey()
-  },
-  headers: {"Access-Control-Allow-Origin": "*"}
+  }
 });
